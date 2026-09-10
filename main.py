f301b6f34135be226e5e508aa7bdc2ba05dfc856
TXRibeiro <tomasxavierribeiro@gmail.com>
2026-09-10 17:48:32 +0100

    Initial Commit: PDF with lines

---
+++ main.py
@@ -0,0 +1,43 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    #Set header
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, text=row["Topic"], align= "L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+    for k in range(10, 270, 10):
+        pdf.line(10, k + 21, 200, k + 21)
+
+    #Set footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, text=row["Topic"], align="R")
+
+
+
+    for i in range(1, row["Pages"]):
+
+        pdf.add_page()
+        pdf.ln(10)
+
+        for j in range(10,290, 10):
+            pdf.line(10, j, 200, j)
+
+
+        pdf.ln(267)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, text=row["Topic"], align="R")
+
+pdf.output("output.pdf")
